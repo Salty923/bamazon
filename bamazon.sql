89d4612c99d7bab511a65309bbc94bdebd7a92c1
Scott Saltzman <salty923@gmail.com>
2018-02-14 20:29:11 -0600

    query to database inventory working

---
+++ bamazon.sql
@@ -0,0 +1,37 @@
+
+INSERT INTO products(product_name, department_name, price, stock_quanity)
+VALUES ("Football", "Sporting Goods", 23, 7);
+
+INSERT INTO products(product_name, department_name, price, stock_quanity)
+VALUES ("Advil", "Pharmacy", 8.50, 15);
+
+INSERT INTO products(product_name, department_name, price, stock_quanity)
+VALUES ("Tent", "Sporting Goods", 175, 3);
+
+INSERT INTO products(product_name, department_name, price, stock_quanity)
+VALUES ("Baseball", "Sporting Goods", 8, 30);
+
+
+INSERT INTO products(product_name, department_name, price, stock_quanity)
+VALUES ("Sledgehammer", "Tools", 17, 5);
+
+INSERT INTO products(product_name, department_name, price, stock_quanity)
+VALUES ("Barbie", "Toys", 6, 10);
+
+
+INSERT INTO products(product_name, department_name, price, stock_quanity)
+VALUES ("Stereo", "Audio", 125, 3);
+
+
+INSERT INTO products(product_name, department_name, price, stock_quanity)
+VALUES ("Coloring Book", "Books", 3, 2);
+
+INSERT INTO products(product_name, department_name, price, stock_quanity)
+VALUES ("Minions", "Movies", 19.99, 8);
+
+
+
+
+
+
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,11 +11,43 @@ const connection = mysql.createConnection({
     database: 'bamazon'
 });
 
-connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
-});
 
-connection.end();
+start();
+
+
+function start() {
+    inquirer
+        .prompt({
+            name: "Start",
+            type: "rawlist",
+            message: "Are you ready to shop at Bamazon??",
+            choices: ["Yes", "No"]
+        })
+        .then(function (answer) {
+            // based on their answer, either call the bid or the post functions
+            if (answer.Start.toUpperCase() === "NO") {
+                start();
+            }
+            else {
+                console.log("Great! Please see out inventory below.");
+                inventory();
+            }
+        });
+
+}
+
+
+function inventory() {
+    console.log("Showing all products...\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        for (var i = 0; i < res.length; i++) {
+            console.log(`${res[i].id}:${res[i].product_name} $${res[i].price} ${res[i].stock_quanity} in stock.`);
+        }
+
+        connection.end();
+    });
+}
+
